fe13977c023999813a604c9da0cb04db01357be2
Mohit Vashishtha <mohit.vashishtha6284@gmail.com>
2023-02-09 17:31:07 -0500

    page template methods

---
+++ src/enums/pageItemTemplateType.ts
@@ -0,0 +1,4 @@
+export enum PageItemTemplateType {
+    Variable = 0,
+	FixedModule = 1
+}
---
+++ src/models/pageModel.ts
@@ -0,0 +1,15 @@
+import { ContentSectionDefinition } from "./contentSectionDefinition";
+
+export class PageModel{
+    doesPageTemplateHavePages: boolean;
+    pageTemplateID: number | null;
+    digitalChannelTypeID: number | null;
+    digitalChannelTypeName: string | null;
+    agilityCode: boolean;
+    pageTemplateName: string | null;
+    relativeURL: string | null;
+    pageNames: string | null;
+    isDeleted: boolean;
+    previewUrl: string | null;
+    contentSectionDefinitions: (ContentSectionDefinition | null)[] | null;
+}
