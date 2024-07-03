4b3b44eced863161b7ccd4b37d6bb8ab88b6e456
Mohit Vashishtha <mohit.vashishtha6284@gmail.com>
2024-07-03 06:24:21 -0400

    content list post method

---
+++ src/models/contentListFilterModel.ts
@@ -0,0 +1,30 @@
+export class ContentListFilterModel {
+    sortIDs: number[];
+    modifiedByIds: number[];
+    dateRange: DateRangeFilter | null;
+    stateIds: number[];
+    fieldFilters: FieldFilter[];
+    genericSearch: string;
+}
+
+export class DateRangeFilter {
+    startDate: string | null;
+    endDate: string | null;
+}
+
+export class NumRangeFilter {
+    fromNum: number;
+    toNum: number;
+}
+
+export class FieldFilterValue {
+    stringValue: string | null;
+    dateRangeValue: DateRangeFilter | null;
+    numRangeValue: NumRangeFilter | null;
+    boolValue: boolean | null;
+}
+
+export class FieldFilter {
+    field: string | null;
+    value: FieldFilterValue | null;
+}
