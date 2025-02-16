624a9a9e60ba05c7a2758a52bb430b93e05073d1
Aaron <aaron@agilitycms.com>
2025-02-15 20:22:15 -0500

    getLocales

---
+++ src/apiMethods/instanceMethods.ts
@@ -0,0 +1,24 @@
+import { Options } from "../models/options";
+import { ClientInstance } from "./clientInstance";
+import { Locales } from "../models/locales";
+import { Exception } from "../models/exception";
+
+export class InstanceMethods {
+    _options!: Options;
+    _clientInstance!: ClientInstance;
+
+    constructor(options: Options) {
+        this._options = options;
+        this._clientInstance = new ClientInstance(this._options);
+    }
+
+    async getLocales(guid: string) {
+        try {
+            let apiPath = `instance/${guid}/locales`;
+            const resp = await this._clientInstance.executeGet(apiPath, guid, this._options.token);
+            return resp.data as Locales[];
+        } catch (err) {
+            throw new Exception("Unable to retrieve locales.", err);
+        }
+    }
+}
---
+++ src/models/locales.ts
@@ -0,0 +1,5 @@
+export class Locales extends Array<string> {
+    constructor(...items: string[]) {
+        super(...items);
+    }
+}
